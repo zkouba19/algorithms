5217ae486bee7bfa5c2f16303272e0837843480e
zkouba <zkouba@me.com>
2017-04-19 13:07:49 -0700

    adding notes & functions to stacks and queues

--- stacks/stack.js
+++ stacks/stack.js
@@ -1,11 +1,29 @@
+
+
 /////////////////////////////////////////////
-//////////// SLL Stacks ////////////////////
+//////////// Stack Stacks ////////////////////
 ////////////////////////////////////////////
 
+///////////////////////////////////////////////
+//////////////// SLL Stack Class //////////////
 
-var StackList1 = new SLL()
-var StackList2 = new SLL()
-SLL.prototype.push = function(val){
+function SLLStack(){
+	this.head = null;
+	this.tail = null;
+}
+///////////////////////////////////////////////
+//////////////// Node Stack Class //////////////
+function Node(value){
+	this.val = value;
+	this.next = null;
+}
+
+
+
+
+var StackList1 = new SLLStack()
+var StackList2 = new SLLStack()
+SLLStack.prototype.push = function(val){
 	var newNode = new Node(val);
 	this.tail = newNode;
 	if(!this.head){
@@ -20,7 +38,7 @@ SLL.prototype.push = function(val){
 	return this;
 }
 
-SLL.prototype.pop = function(){
+SLLStack.prototype.pop = function(){
 	if(!this.head) return this;
 	if(!this.head.next){
 		this.head = null;
@@ -36,7 +54,7 @@ SLL.prototype.pop = function(){
 	return this;
 }
 
-SLL.prototype.show = function(){
+SLLStack.prototype.show = function(){
 	var curr = this.head;
 	while(curr){
 		console.log(curr.val)
@@ -48,7 +66,7 @@ SLL.prototype.show = function(){
 /////// we could just return this.tail.val. However, I will show how to /////
 /////// iterate through the stack to get the value.	/////////////////////////
 ////////////////////////////////////////////////////////////////////////////
-SLL.prototype.returnTopValue = function(){
+SLLStack.prototype.returnTopValue = function(){
 	// return this.tail.val
 
 	// or
@@ -61,12 +79,12 @@ SLL.prototype.returnTopValue = function(){
 	return curr.val;
 }
 
-SLL.prototype.IsEmpty = function(){
+SLLStack.prototype.IsEmpty = function(){
 	if(!this.head) return true;
 	else return false;
 }
 
-SLL.prototype.Size = function(){
+SLLStack.prototype.Size = function(){
 	var count = 0
 	if(!this.head) return count;
 	var curr = this.head;
@@ -95,27 +113,91 @@ function CompareStacks(ST1, ST2){
 }
 
 
+// StackList1.push(10);
+// StackList1.push(15);
+// StackList1.push(9);
+// StackList1.push(21);
+// StackList1.push(21);
+
+// StackList2.push(10);
+// StackList2.push(15);
+// StackList2.push(9);
+// StackList2.push(21);
+// StackList2.push(21);
+
+// console.log(StackList1);
+// StackList1.show();
+// // StackList1.pop();
 
 
-StackList1.push(10);
-StackList1.push(15);
-StackList1.push(9);
-StackList1.push(21);
-StackList1.push(21);
-
-StackList2.push(10);
-StackList2.push(15);
-StackList2.push(9);
-StackList2.push(21);
-StackList2.push(21);
-
-console.log(StackList1);
-StackList1.show();
-// StackList1.pop();
+// console.log(StackList1);
+// StackList1.show();
+// console.log(StackList1.returnTopValue());
+// console.log(StackList1.IsEmpty());
+// console.log(CompareStacks(StackList1, StackList2));
 
 
-console.log(StackList1);
-StackList1.show();
-console.log(StackList1.returnTopValue());
-console.log(StackList1.IsEmpty());
-console.log(CompareStacks(StackList1, StackList2));
+////////////////////////////////////////////////
+/////////////// arrStack Class /////////////////
+function StackArray(){
+	this.size = 0;
+	this.stack = [];
+};
+
+////////////////////////////////////////////
+/////////// prototype functions ////////////
+
+/////////////////////////////////////////////
+///// add value to the stack at the top /////
+/////////////////////////////////////////////
+StackArray.prototype.push = function(val){
+	this.stack[this.size] = val;
+	this.size += 1;
+	return this;
+}
+
+/////////////////////////////////////////////
+//////// remove and return top value ////////
+/////////////////////////////////////////////
+StackArray.prototype.pop = function(){
+	this.size = this.size-1;
+	return this.stack.pop();
+}
+
+//////////////////////////////////////////////////
+//// return the value at the top of the stack ////
+//////////////////////////////////////////////////
+StackArray.prototype.top = function(){
+	return this.stack[this.size-1];
+}
+
+//////////////////////////////////////////////////
+//// given a value search the stack & return /////
+//// boolean wether value was found ro not ///////
+//////////////////////////////////////////////////
+StackArray.prototype.contains = function(val){
+	for(var i = this.size-1; i >= 0; i--){
+		if(this.stack[i] == val){
+			return true;
+		}
+	}
+	return false;
+}
+
+///////////////////////////////
+//////////// tests ////////////
+///////////////////////////////
+
+// var stack1 = new StackArray();
+// stack1.push(5);
+// stack1.push(3);
+// stack1.push(6);
+// stack1.push(2);
+// console.log(stack1.pop());
+// console.log(stack1)
+// console.log(stack1.top())
+// console.log(stack1.size);
+
+
+
+
